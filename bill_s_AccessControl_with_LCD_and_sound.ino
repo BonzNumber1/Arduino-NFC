840ff9e91956cb43e8a99ef291d5228ef0f417ef
BonzNumber1 <hybrid_systems@hotmail.com>
2021-12-13 11:43:04 -0600

    Update bill_s_AccessControl_with_LCD_and_sound.ino

--- bill_s_AccessControl_with_LCD_and_sound.ino
+++ bill_s_AccessControl_with_LCD_and_sound.ino
@@ -47,17 +47,32 @@
 
    @license Released into the public domain.
 
-   Typical pin layout used:
-   -----------------------------------------------------------------------------------------
-               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
-               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
-   Signal      Pin          Pin           Pin       Pin        Pin              Pin
-   -----------------------------------------------------------------------------------------
-   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
-   SPI SS      SDA(SS)      10            53        D10        10               10
-   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
-   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
-   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+      Pin layout used For MFRC522:
+   ---------------------------------------
+               MFRC522      Arduino      
+               Reader/PCD   Uno   
+   Signal      Pin          Pin          
+   ---------------------------------------
+   RST/Reset   RST          9             
+   SPI SS      SDA(SS)      10            
+   SPI MOSI    MOSI         11
+   SPI MISO    MISO         12
+   SPI SCK     SCK          13
+   
+   
+      Pin layout used For 16x2 1602A:
+   ---------------------------------------
+               16x2         Arduino      
+               1602a        Uno   
+   Signal      Pin          Pin          
+   ---------------------------------------
+   D7          14           2             
+   D6          13           3            
+   D5          12           4
+   D4          11           5
+   EN          6            6
+   RS          4            7
+*/
 */
 
 #include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
